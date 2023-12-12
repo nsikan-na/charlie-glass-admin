5e02e6ed8ebc0c8a3a2f411929dbe5438b71de53
nsikan.akpan <nsikan.akpan@betmakers.com>
2023-12-12 00:53:07 -0500

    fixed deploy

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,7 +2,7 @@ import Invoice from "../screens/invoice/Invoice";
 import Layout from "../layout/Layout";
 import PdfTemplate from "../util/pdf/PdfTemplate";
 import PDfDownloadButton from "../util/pdf/PdfDownloadButton";
-import Login from "../screens/Login/LogIn";
+import Login from "../screens/login/LogIn";
 
 export const routes = [
   {
