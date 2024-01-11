cd76e7b7dd9ff794abdaea02859db45ee5298873
shawnm4 <shawnm18057310@gmail.com>
2024-01-10 22:02:42 -0500

    set user state to initial state

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 
 export const useInitialStore = () => {
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<any>(userInitalState);
 
   return {
     user,
@@ -19,7 +19,8 @@ export const useInitialStore = () => {
 };
 
 export const userInitalState = {
-  userId: null,
+  //Remove this
+  userId: 1,
   userName: null,
   expirationMs: null,
   accessToken: null,
@@ -35,12 +36,7 @@ export const Context = createContext<TContextProps>({
     zip: null,
     email: null,
   },
-  user: {
-    userId: null,
-    userName: null,
-    expirationMs: null,
-    accessToken: null,
-  },
+  user: userInitalState,
 });
 
 type TContextProps = {
