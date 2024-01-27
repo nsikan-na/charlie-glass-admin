a45a5f5bf5f60801281868b3f70079b9cf8c81da
Shawnm4 <139506398+Shawnm4@users.noreply.github.com>
2024-01-27 03:57:36 -0500

    New shawn branch (#15)

--- src/layout/TopNavBar.tsx
+++ src/layout/TopNavBar.tsx
@@ -6,13 +6,31 @@ import setLocalStorage from "../hooks/localstorage/setLocalStorage";
 import ELocalStorage from "../util/enums/localStorage";
 import { ERoute } from "../util/enums/routes";
 import { EColors } from "../util/enums/colors";
-import { UserOutlined } from "@ant-design/icons";
+import { LogoutOutlined, UserOutlined, DownOutlined } from "@ant-design/icons";
+import { MenuProps, Dropdown, Space } from "antd";
 
 const { Header } = Layout;
 
 const TopNavBar = () => {
   const { user }: any = useContext(Context);
   const navigate = useNavigate();
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: (
+        <span
+          className="cursor-pointer  "
+          onClick={() => {
+            setLocalStorage(ELocalStorage.USER, userInitialState);
+            navigate(ERoute.LOGIN);
+          }}
+        >
+          <LogoutOutlined className="mr-2" />
+          Log Out
+        </span>
+      ),
+    },
+  ];
 
   return (
     <Header
@@ -30,24 +48,22 @@ const TopNavBar = () => {
           Charlie Glass Admin
         </div>
         <div>
-          <span className="mr-4">
-            <Avatar
-              size="small"
-              className="mr-1 self-center"
-              style={{ backgroundColor: EColors.primary }}
-              icon={<UserOutlined style={{ color: EColors.white }} />}
-            />
-            {`${user.userName}`}
-          </span>
-          <span
-            className="cursor-pointer"
-            onClick={() => {
-              setLocalStorage(ELocalStorage.USER, userInitialState);
-              navigate(ERoute.LOGIN);
-            }}
-          >
-            Log Out
-          </span>
+          <Dropdown menu={{ items }} className="mb-2 mt-4">
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                <span className="">
+                  <Avatar
+                    size="small"
+                    className="mr-1 self-center"
+                    style={{ backgroundColor: EColors.primary }}
+                    icon={<UserOutlined style={{ color: EColors.white }} />}
+                  />
+                  {`${user.userName}`}
+                </span>
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
         </div>
       </div>
     </Header>
