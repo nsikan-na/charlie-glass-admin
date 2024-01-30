11c4732e1688afdce373240537e0a918e77d9b8e
shawnm4 <shawnm18057310@gmail.com>
2024-01-30 13:01:39 -0500

    Designed dashboard

--- src/screens/invoice/dashboard/Dashboard.tsx
+++ src/screens/invoice/dashboard/Dashboard.tsx
@@ -7,6 +7,8 @@ import dayjs from "dayjs";
 import useQueryParam from "../../../hooks/queryParam/useQueryParam";
 import { invoiceTabKey } from "../Main";
 import { formatDayjsDate } from "../../../util/helpers";
+import { EColors } from "../../../util/enums/colors";
+import { BarChartOutlined } from "@ant-design/icons";
 
 const initialState = {
   fromDate: formatDayjsDate(dayjs().subtract(1, "month")),
@@ -31,26 +33,58 @@ export default function Dashboard() {
     }));
   };
   return (
-    <div>
-      <RangePicker
-        onChange={onRangeFilterChange}
-        value={
-          filters?.fromDate && filters?.toDate
-            ? [dayjs(filters?.fromDate), dayjs(filters?.toDate)]
-            : undefined
-        }
-      />
-      <span className="ml-2">
-        <SearchButton onClick={() => setInput(filters)} />
-      </span>
-      <div className="flex justify-center">
-        <div className="w-full">
-          <ProfitsWidget input={input} />
-        </div>
-        <div className="w-full">
-          <ServicesWidget input={input} />
+    <>
+      <div className=" mb-12 ">
+        <RangePicker
+          className="  "
+          onChange={onRangeFilterChange}
+          value={
+            filters?.fromDate && filters?.toDate
+              ? [dayjs(filters?.fromDate), dayjs(filters?.toDate)]
+              : undefined
+          }
+        />
+        <span className="ml-2">
+          <SearchButton onClick={() => setInput(filters)} />
+        </span>
+      </div>
+      <div className="  w-full h-screen rounded-lg">
+        <div className="flex justify-center gap-14 ">
+          <div
+            style={{ borderColor: `${EColors.primary}` }}
+            className="border-t-8  bg-white rounded-lg drop-shadow-2xl w-2/5 mt-12"
+          >
+            <div className="ml-8 mb-2 flex gap-2 text-lg">
+              <BarChartOutlined style={{ color: EColors.primary }} />
+              <div>Profits</div>
+            </div>
+            <div className="flex justify-center ">
+              <div
+                style={{ borderColor: `${EColors.primary}` }}
+                className="border-b-2 w-11/12  "
+              ></div>
+            </div>
+            <ProfitsWidget input={input} />
+          </div>
+          <div
+            style={{ borderColor: `${EColors.primary}` }}
+            className="= border-t-8  bg-white rounded-lg drop-shadow-2xl w-2/5 mt-12 "
+          >
+            <div className=" ml-8 mb-2 flex gap-2 text-lg">
+              <BarChartOutlined style={{ color: EColors.primary }} />
+              <div>Services</div>
+            </div>
+
+            <div className="flex justify-center ">
+              <div
+                style={{ borderColor: `${EColors.primary}` }}
+                className="border-b-2 w-11/12  "
+              ></div>
+            </div>
+            <ServicesWidget input={input} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
