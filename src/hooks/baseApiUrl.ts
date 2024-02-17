1c0f9e77a3826dd71d99e5bc7bd462bb23f123d7
Nsikan Akpan <nsikan.akpan@betmakers.com>
2024-02-17 16:08:24 -0500

    changed url

--- src/hooks/baseApiUrl.ts
+++ src/hooks/baseApiUrl.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
   "https://no-cope-apis-04f7f012003b.herokuapp.com/cgi-admin";
-// "https://charlie-glass-admin-api.vercel.app/cgi-admin";
+// "https://no-cope-apis.vercel.app/cgi-admin";
